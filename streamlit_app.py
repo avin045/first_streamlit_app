931b535f4322050ecadbf6928c49bf5186a99081
avin045 <78691324+avin045@users.noreply.github.com>
2023-01-15 10:35:21 +0530

    Updated with Header,Text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,11 @@
 import streamlit
+# TITLE
 streamlit.title('My Parents New Healthy Diner');
+
+# HEADER
+streamlit.header('Breakfast Menu');
+
+# TEXT
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('Kale, Spinach & Rocket Smoothie');
+streamlit.text('Hard-Boiled Free-Range Egg');
