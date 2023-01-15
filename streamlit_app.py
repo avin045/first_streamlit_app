ccc71290a4a1e80b923de654a75c233bdf774006
avin045 <78691324+avin045@users.noreply.github.com>
2023-01-15 10:44:55 +0530

    Changed Title and Added New Header.
     Altered Old header and text.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit
 # TITLE
-streamlit.title('My Parents New Healthy Diner');
+streamlit.title('My Mom\'s New Healthy Diner');
 
 # HEADER
-streamlit.header('Breakfast Menu');
+streamlit.header('Breakfast Favorites');
 
 # TEXT
-streamlit.text('Omega 3 & Blueberry Oatmeal');
-streamlit.text('Kale, Spinach & Rocket Smoothie');
-streamlit.text('Hard-Boiled Free-Range Egg');
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal');
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie');
+streamlit.text('🐔 Hard-Boiled Free-Range Egg');
+streamlit.text('🥑🍞 Avocado Toast');
+
+# Another HEADER
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
