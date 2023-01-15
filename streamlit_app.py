7fa8713d3df59a1640e994618df2c060e6ef73b6
avin045 <78691324+avin045@users.noreply.github.com>
2023-01-15 10:48:00 +0530

    Added Dataframe via S3 file

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,9 @@ streamlit.text('🥑🍞 Avocado Toast');
 
 # Another HEADER
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇');
+
+
+# IMPORTING PANDAS
+import pandas as pd
+my_fruits_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruits_list);
