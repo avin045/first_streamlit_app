dfc0f7cf6309a88dfe33bbb3df5ea6873d81ff34
avin045 <78691324+avin045@users.noreply.github.com>
2023-01-17 14:17:14 +0530

    USER INPUT FOR REQUEST

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,13 @@ streamlit.dataframe(fruits_to_show);
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!");
 
-# RESPONSE from REQUESTS
+# -------------------------------------------------- USER INPUT FOR REQUEST -------------------------------------------------- #
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+# -------------------------------------------------- RESPONSE from REQUESTS -------------------------------------------------- #
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi");
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice);
 # streamlit.text(fruityvice_response)
 # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
 
@@ -50,4 +54,5 @@ fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 
 # Display the Normalized(structured) Data
 streamlit.dataframe(fruityvice_normalized)
+
 
