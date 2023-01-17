64217ef909f957c539a4b1162bfcd4fae73ec8e9
avin045 <78691324+avin045@users.noreply.github.com>
2023-01-17 14:02:59 +0530

    Response 200 to JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,5 @@ streamlit.header("Fruityvice Fruit Advice!");
 
 # RESPONSE from REQUESTS
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+# streamlit.text(fruityvice_response)
+streamlit. text ( fruityvice_response.json())
